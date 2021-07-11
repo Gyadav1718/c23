1b5f0fd4cc81b1022b68c80581032a5fca6cf713
Gyadav1718 <86192597+Gyadav1718@users.noreply.github.com>
2021-07-11 18:59:07 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,16 @@
+class Box{
+constructor(){
+    box_options={
+        restitution: 1
+    }
+    this.body=Bodies.rectangle(200,100,50,50,box_options);
+    this.width=50;
+    this.height=50;
+    World.add(myWorld,this.body)
+}
+display(){
+    var pos=this.body.position;
+    rectMode(CENTER);
+    rect(pos.x,pos.y,this.width,this.height);
+}
+}
